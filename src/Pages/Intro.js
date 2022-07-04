ce0cbb73a18b46e44ff8d0ab9ccda75fbfc0e032
eetukarttunen <eetu.karttunen@outlook.com>
2022-07-04 09:06:23 +0300

    added new icons & added typewriter

--- src/Pages/Intro.js
+++ src/Pages/Intro.js
@@ -1,37 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./Intro.css";
-import 'font-awesome/css/font-awesome.min.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faReact, faNodeJs, faJava, faPython, faPhp, faWordpressSimple, faDocker } from "@fortawesome/free-brands-svg-icons"
+import TypeWriterEffect from 'react-typewriter-effect';
 
-/*
- <div className='icons-container'>
-          <img src="../logo512.png"></img>
-          <img src="../logo512.png"></img>
-          <img src="../logo512.png"></img>
-        </div>
-
-*/
 function Intro() {
   
   return (
     <div className="intro">
       <div className="intro-left">
         <h1 className="name">Eetu Karttunen.</h1>
-        <h3 className='title'><div className='developer-title'>Software Developer</div></h3>
-        
-        <div className='icons-container'>
-        
-        <FontAwesomeIcon icon={ faReact } />
-        <FontAwesomeIcon icon={ faNodeJs } />
-        <FontAwesomeIcon icon={ faJava } />
-        <FontAwesomeIcon icon={ faPython } />
-        <FontAwesomeIcon icon={ faPhp } />
-        <FontAwesomeIcon icon={ faWordpressSimple } />
-        <FontAwesomeIcon icon={ faDocker } />
-
+        <div className='developer-title'>
+          <TypeWriterEffect
+          textStyle={{
+            color: 'rgb(59, 157, 209)',
+            fontWeight: 500,
+            fontSize: '20px',
+            'text-align': 'center',
+          }}
+          cursorColor="#3F3D56"
+          multiText={[
+            'Software Specialist',
+          ]}
+          multiTextDelay={2000}
+          typeSpeed={80}
+          hideCursorAfterText={true}
+          multiTextLoop={false}
+          />
         </div>
-
       </div>
     </div>
   );
