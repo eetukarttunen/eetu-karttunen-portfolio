185f711e8323464795e607cf5cf0661534204037
Eetu Karttunen <eetu.karttunen@outlook.com>
2023-02-10 13:08:22 +0200

    added email icon and styling

--- src/Components/Sandbox.js
+++ src/Components/Sandbox.js
@@ -10,7 +10,7 @@ function Sandbox() {
       <h1>Sandbox</h1>
 
       <p>Welcome to the Sandbox page!</p>
-      <p style={{textAlign: "left"}}>This is where I showcase and test different React components that I have coded myself. This page serves as a playground for me to experiment with new ideas, try out different design concepts, and bring my visions to life. Whether you are a fellow developer or just someone who is interested in what I do, I hope you enjoy exploring this page as much as I enjoyed creating it.</p>
+      <p style={{textAlign: "left", fontWeight: 'lighter'}}>This is where I showcase and test different React components that I have coded myself. This page serves as a playground for me to experiment with new ideas, try out different design concepts, and bring my visions to life. Whether you are a fellow developer or just someone who is interested in what I do, I hope you enjoy exploring this page as much as I enjoyed creating it.</p>
       
       <img
         src="./icons/dummy.jpg"
